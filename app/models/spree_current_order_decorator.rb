15cf675f2de3ed56186ed9cb4a37db315c15c15c
citrus <spencer@citrusme.com>
2011-01-12 16:47:01 -0800

    removed logging

--- app/models/spree_current_order_decorator.rb
+++ app/models/spree_current_order_decorator.rb
@@ -25,30 +25,14 @@ module Spree
 
     # The current incomplete order from the session for use in cart and during checkout
     def current_order(create_order_if_necessary = false)
-      puts "----"
-      
-      puts "CURRENT ORDER:"
-      puts @current_order.inspect
-    
-      return @current_order if @current_order
-      
-      puts "SETTING CURRENT ORDER"
       @current_order = Order.find_by_id(session[:order_id], :include => :adjustments)
-      
       if create_order_if_necessary and (@current_order.nil? or @current_order.completed?)
         @current_order = Order.new
         before_save_new_order
         @current_order.save!
-        puts "ORDER CREATED!"  
       end
       
-      
-      if current_user 
-      
-        puts current_user.inspect
-        puts current_user.has_role?("wholesaler")
-        puts @current_order.is_wholesale?
-      
+      if current_user && @current_order
         if current_user.has_role?("wholesaler") && !@current_order.is_wholesale?
           @current_order.to_wholesale!
         elsif !current_user.has_role?("wholesaler") && @current_order.is_wholesale?
@@ -56,10 +40,7 @@ module Spree
         end
       end
       
-      
       session[:order_id] = @current_order ? @current_order.id : nil
-      
-      puts @current_order.inspect
       
       @current_order
     end
