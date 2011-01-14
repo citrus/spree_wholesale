3d6c4cdb8dc63a1b942c7f002162fa723a8f537a
citrus <spencer@citrusme.com>
2011-01-14 12:14:26 -0800

    added wholesaler test

--- spec/test_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestApp::Application.config.secret_token = '9dee470534624173b82f2f05e363f2daa469f19a3764c1fa98ca0d7181347d8797bcff744ea64111065c8876d4cf80a25949300c4a62d95ddac6cb336cb6a555'
