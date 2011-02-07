c32ddd489b59ce8c6f3632bcbdfefb25ac5cc3ab
citrus <spencer@citrusme.com>
2011-02-07 12:22:39 -0800

    moved ability to lib and added pre 0.40.2 logic

--- lib/spree_wholesale.rb
+++ lib/spree_wholesale.rb
@@ -6,6 +6,7 @@ end
 
 require 'spree_wholesale_hooks'
 require 'spree_wholesale/wholesaler_controller'
+  
 
 module SpreeWholesale  
   class Engine < Rails::Engine
@@ -16,9 +17,18 @@ module SpreeWholesale
       app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{config.root}/public"
     end
 
+    def self._load(c)
+      Rails.env.production? ? require(c) : load(c)
+    end
+
     def self.activate
+    
+      if 40 <= Spree.version.split(".")[1].to_i
+        _load File.expand_path("../spree_wholesale/wholesaler_ability.rb", __FILE__)
+      end
+    
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        _load(c)
       end
     end
 
