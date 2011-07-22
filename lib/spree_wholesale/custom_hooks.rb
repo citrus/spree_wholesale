5ffeb3010f848cb29935f25cc29a32808776eb91
Cameron Carroll <ckcarroll4@gmail.com>
2011-07-22 14:00:36 -0700

    Testing first deface override

--- lib/spree_wholesale/custom_hooks.rb
+++ lib/spree_wholesale/custom_hooks.rb
@@ -1,4 +1,5 @@
 module SpreeWholesale
+  require 'deface'
   class CustomHooks < Spree::ThemeSupport::HookListener
         
     replace :cart_item_price,                 'hooks/cart_item_price'
@@ -14,7 +15,11 @@ module SpreeWholesale
     insert_after :admin_orders_index_headers, 'admin/hooks/admin_orders_index_headers'
     insert_after :admin_orders_index_rows,    'admin/hooks/admin_orders_index_rows'
     insert_after :admin_orders_index_search,  'admin/hooks/admin_orders_index_search'
-    insert_after :admin_tabs,                 'admin/hooks/wholesale_tab'
+    # insert_after :admin_tabs,                 'admin/hooks/wholesale_tab'
+    Deface::Override.new(:virtual_path => 'layouts/admin',
+                          :name => "admin-wholesale-tab",
+                          :insert_bottom => "[data-hook='admin_tabs']",
+                          :partial => "admin/hooks/wholesale_tab")
     
   end
 end
