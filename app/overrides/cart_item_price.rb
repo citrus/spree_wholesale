56a662c633c26878a30d681a9221fae431efe691
John Hwang <johnyhwang@gmail.com>
2012-01-16 10:39:17 -0500

    Reformatting

--- app/overrides/cart_item_price.rb
+++ app/overrides/cart_item_price.rb
@@ -1,6 +1,8 @@
 #replace :cart_item_price,                 'hooks/cart_item_price'
-Deface::Override.new(:virtual_path => 'orders/_line_item',
-:name => 'cart_item_price',
-:replace_contents => "[data-hook='cart_item_price'], #cart_item_price[data-hook]",
-:partial => "hooks/cart_item_price",
-:disabled => false)
+Deface::Override.new(
+  :virtual_path => 'orders/_line_item',
+  :name => 'cart_item_price',
+  :replace_contents => "[data-hook='cart_item_price'], #cart_item_price[data-hook]",
+  :partial => "hooks/cart_item_price",
+  :disabled => false
+)
