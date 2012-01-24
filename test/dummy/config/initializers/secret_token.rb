406d401dd83243e6b28c79ac880ffc3ca82c0125
Spencer Steffen <spencer@citrusme.com>
2012-01-24 11:06:09 -0800

    remove test/dummy from git since we`re using dummier

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '2bd20f19f127e4e7d193e79c8ec51f1730dc9496818491ac4020fccfe0a40fcc02eeb3304f253c7b58103686b59ab49d20e119d6fc38afc3f8e4077488b225a5'
