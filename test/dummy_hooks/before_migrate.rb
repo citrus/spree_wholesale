053dc5b6d45ed8397162356cc99fb702abf4d275
Spencer Steffen <spencer@citrusme.com>
2012-01-24 11:50:28 -0800

    fix dummy hooks and add tweak install generator

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,2 +1,13 @@
-rake "spree_core:install spree_auth:install spree_sample:install"
-run "rails g spree_wholesale:install"
+run "rails g spree:site"
+
+# remove all stylesheets except core  
+%w(admin store).each do |ns|
+  js  = "app/assets/javascripts/#{ns}/all.js"
+  css = "app/assets/stylesheets/#{ns}/all.css"
+  remove_file js
+  remove_file css
+  template "#{ns}/all.js", js
+  template "#{ns}/all.css", css
+end
+
+run "rails g spree_wholesale:install --skip_migrations"
