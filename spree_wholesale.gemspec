315cec47237ae9bf64c1a90e20db486d4d8fcdc7
Spencer Steffen <spencer@citrusme.com>
2012-01-25 10:27:36 -0800

    remove spork dev dependency

--- spree_wholesale.gemspec
+++ spree_wholesale.gemspec
@@ -25,9 +25,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency('shoulda',          '>= 3.0.0.beta2')
   s.add_development_dependency('factory_girl',     '>= 2.4.2')
 	s.add_development_dependency('capybara',         '>= 1.1.1')	
-	s.add_development_dependency('database_cleaner', '>= 0.6.7')
-	s.add_development_dependency('spork',            '>= 0.9.0.rc9')
-  s.add_development_dependency('spork-testunit',   '>= 0.0.5')
 	s.add_development_dependency('sqlite3',          '>= 1.3.4')
   s.add_development_dependency('dummier',          '~> 0.2.4')
   
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,60 +1,49 @@
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
-require 'spork'
 
-Spork.prefork do
-
-  env = File.expand_path("../dummy/config/environment.rb",  __FILE__)
-  if File.exists?(env)
-    require env
-  else
-    raise LoadError, "Please create the dummy app before running tests. Try running `bundle exec dummier`"
-  end
-  
-  require 'rails/test_help'
-  require 'shoulda'
-  require 'factory_girl'
-  require 'capybara/rails'
-  require 'database_cleaner'
-
-  # Load support files
-  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-  DatabaseCleaner.strategy = :truncation
-  
-  include HelperMethods
-
-  ActionMailer::Base.delivery_method    = :test
-  ActionMailer::Base.perform_deliveries = false
-  ActionMailer::Base.default_url_options[:host] = "example.com"
-
-  Rails.backtrace_cleaner.remove_silencers!
-
-  # Configure capybara for integration testing
-  Capybara.default_driver    = :selenium
-  Capybara.default_selector  = :css
-  
-  # Run any available migration
-  ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
-  
-  class ActionController::TestCase
-    include Devise::TestHelpers
-  end
-    
-  class ActiveSupport::TestCase
-    self.fixture_path = File.expand_path('../fixtures', __FILE__)
-    fixtures :all
-  end
-  
-  class ActionController::IntegrationTest
-    self.fixture_path = File.expand_path('../fixtures', __FILE__)
-  end
-  
-  # Default to US
-  Spree::Config.set(:default_country_id => 214)
+env = File.expand_path("../dummy/config/environment.rb",  __FILE__)
+if File.exists?(env)
+  require env
+else
+  raise LoadError, "Please create the dummy app before running tests. Try running `bundle exec dummier`"
 end
 
-Spork.each_run do
-  FactoryGirl.reload
-  Rails.application.reload_routes!  
+require 'rails/test_help'
+require 'shoulda'
+require 'factory_girl'
+require 'capybara/rails'
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+include HelperMethods
+
+ActionMailer::Base.delivery_method    = :test
+ActionMailer::Base.perform_deliveries = false
+ActionMailer::Base.default_url_options[:host] = "example.com"
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Configure capybara for integration testing
+Capybara.default_driver    = :selenium
+Capybara.default_selector  = :css
+
+# Run any available migration
+ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
+  
+class ActiveSupport::TestCase
+  self.fixture_path = File.expand_path('../fixtures', __FILE__)
+  fixtures :all
+end
+
+class ActiveSupport::IntegrationTest
+  self.fixture_path = File.expand_path('../fixtures', __FILE__)
+end
+
+# Default to US
+Spree::Config.set(:default_country_id => 214)
+FactoryGirl.reload
